b08156557470f2b50ccb9556a9cde6e5c367b833
bosterma <bjorn.osterman@gritlab.ax>
2024-05-07 20:20:22 +0300

    My Family

--- myfamily.sh
+++ myfamily.sh
@@ -1,5 +1,4 @@
 # This file will tell about my family
 #! /bin/bash
 
-echo "$opt" | tr -d '"'
-curl --location --request GET https://01.gritlab.ax/assets/superhero/all.json | jq '.[]| select ( .id == 1) .relatives'
+curl —s https://01.gritlab.ax/assets/superhero/all.json | jq --arg hero_id “$HERO_ID” ‘.[] | select(.id == ($hero_id | to number)) | .connections.relatives’ | tr -d ‘“‘
