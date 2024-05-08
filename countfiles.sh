ea5f86ea337f1788c3fa52b10c10a1dec076c14a
bosterma <nalle.osterman@gmail.com>
2024-05-08 19:31:04 +0300

    Count Files

---
+++ countfiles.sh
@@ -0,0 +1,5 @@
+# This file will print the number (and only the number)
+# of regular files and folders contained 
+# in the current directory and its sub-folders
+#! /bin/bash
+find $DIR_NAME | wc -l
