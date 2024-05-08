55679fdd4c81b5b4e6edf92cd5e082d616d93fd8
bosterma <nalle.osterman@gmail.com>
2024-05-08 18:59:05 +0300

    My very first function

---
+++ isnegative.go
@@ -0,0 +1,17 @@
+package IsNegative
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) {
+
+	trueval := 'T'
+	falsval := 'F'
+	if nb >= 0 {
+		z01.PrintRune(falsval)
+		z01.PrintRune(10)
+	} else {
+		z01.PrintRune(trueval)
+		z01.PrintRune(10)
+	}
+
+}
