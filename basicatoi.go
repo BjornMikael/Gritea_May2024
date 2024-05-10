8bcc182458099b81e425bd3e09bdf1667017c3a6
bosterma <nalle.osterman@gmail.com>
2024-05-10 08:38:02 -0700

    Basic Atoi

--- basicatoi.go
+++ basicatoi.go
@@ -1,13 +1,19 @@
 package piscine
 
-import "strconv"
-
 func BasicAtoi(s string) int {
-	i, err := strconv.Atoi(s)
 
-	if err == nil {
-		return i
-	} else {
-		return 0
+	arrayStr := []rune(s)
+	n := len(s)
+	ans := 0
+	for i := 0; i < n; i++ {
+		if arrayStr[i] < '0' || arrayStr[i] > '9' {
+
+			return ans
+		} else {
+			ans *= 10
+			ans += int(arrayStr[i]) - '0'
+		}
+
 	}
+	return ans
 }
