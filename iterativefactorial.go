d59f4bb0159da0eaa50fb448dd70bac201750f13
bosterma <nalle.osterman@gmail.com>
2024-05-13 16:33:32 +0300

    Ite

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,16 +1,16 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 12 {
-		return 0
-	} else if nb == 0 {
+	a := 1
+	if nb == 1 || nb == 0 {
 		return 1
+	} else if nb > 1 && nb <= 25 {
+		for i := 1; i <= nb; i++ {
+			a = a * i
+		}
+	} else {
+		return 0
 	}
 
-	c := nb
-	b := 1
-	for nb := 1; nb <= c; nb++ {
-		b = b * nb
-	}
-	return b
+	return a
 }
