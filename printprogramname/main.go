076637716c489cfb3e3643643717e858f850f010
bosterma <nalle.osterman@gmail.com>
2024-05-16 17:35:07 +0300

    New commit Printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,10 +6,26 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
-	arg := os.Args
-	for _, w := range arg[0] {
-		z01.PrintRune(w)
+func printStr(s []rune) {
+	n := 0
+	for n < len(s) {
+		z01.PrintRune(s[n])
+		n++
 	}
+}
+
+func endIndex(s []rune, toFind rune) int {
+	ret := 0
+	for nextIndex := 0; nextIndex < len(s); nextIndex++ {
+		if s[nextIndex] == toFind {
+			ret = nextIndex + 1
+		}
+	}
+	return ret
+}
+
+func main() {
+	arg := []rune(os.Args[0])
+	printStr(arg[endIndex(arg, '/'):])
 	z01.PrintRune('\n')
 }
