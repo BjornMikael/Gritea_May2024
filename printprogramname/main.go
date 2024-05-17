a2cea62da70befc44029be98b256d733875e6c37
bosterma <nalle.osterman@gmail.com>
2024-05-17 17:12:07 +0300

    screwed up old main.go file, refixing it now

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,26 +6,10 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func printStr(s []rune) {
-	n := 0
-	for n < len(s) {
-		z01.PrintRune(s[n])
-		n++
-	}
-}
-
-func endIndex(s []rune, toFind rune) int {
-	ret := 0
-	for nextIndex := 0; nextIndex < len(s); nextIndex++ {
-		if s[nextIndex] == toFind {
-			ret = nextIndex + 1
-		}
-	}
-	return ret
-}
-
 func main() {
-	arg := []rune(os.Args[0])
-	printStr(arg[endIndex(arg, '/'):])
+	arg := os.Args
+	for _, w := range arg[0] {
+		z01.PrintRune(w)
+	}
 	z01.PrintRune('\n')
 }
