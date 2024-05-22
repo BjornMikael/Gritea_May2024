8fdf62fb7782290c005f718426ac1d3a1444c1bf
bosterma <nalle.osterman@gmail.com>
2024-05-22 15:15:23 +0300

    Map; q9t2

--- map.go
+++ map.go
@@ -1,9 +1,13 @@
 package piscine
 
-func Map(f func(int) bool, a []int) []bool {
-	b := make([]bool, len(a))
-	for i, v := range a {
-		b[i] = f(v)
+func Map(f func(int) bool, arr []int) []bool {
+	length := 0
+	for l := range arr {
+		length = l + 1
 	}
-	return b
+	boolAr := make([]bool, length)
+	for i := range arr {
+		boolAr[i] = f(arr[i])
+	}
+	return boolAr
 }
