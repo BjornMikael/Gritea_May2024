b3604017fea61fff389f5ac278efe61c531d561c
bosterma <nalle.osterman@gmail.com>
2024-05-22 18:13:50 +0300

    Doop; q9t6

--- doop/main.go
+++ doop/main.go
@@ -6,44 +6,51 @@ import (
 	"strconv"
 )
 
-func validateOperator(test string) bool {
-	op := []string{"+", "-", "*", "/", "%"}
-	for _, res := range op {
-		if res == test {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
-	args := os.Args[1:]
-	if len(args) > 3 || len(args) < 3 {
-		fmt.Print()
-	} else {
-		if validateOperator(args[1]) == false {
-			fmt.Println(0)
-		} else {
-			premier, _ := strconv.Atoi(args[0])
-			second, _ := strconv.Atoi(args[2])
-
-			if args[1] == "%" && second == 0 {
-				fmt.Print("No Modulo by 0\n")
-			} else if args[1] == "/" && second == 0 {
-				fmt.Print("No division by 0\n")
-			} else if args[1] == "+" {
-				fmt.Println(premier + second)
-			} else if args[1] == "-" {
-				fmt.Println(premier - second)
-			} else if args[1] == "*" {
-				fmt.Println(premier * second)
-			} else if args[1] == "/" {
-				fmt.Println(premier / second)
-			} else {
-				fmt.Println(premier % second)
-			}
-
-		}
+	if len(os.Args) != 4 {
+		return
 	}
 
+	val1, err1 := strconv.ParseInt(os.Args[1], 10, 64)
+	operator := os.Args[2]
+	val2, err2 := strconv.ParseInt(os.Args[3], 10, 64)
+
+	if err1 != nil || err2 != nil {
+		return
+	}
+
+	switch operator {
+	case "+":
+		result := val1 + val2
+		if (val1 > 0 && val2 > 0 && result < 0) || (val1 < 0 && val2 < 0 && result > 0) {
+			return // Overflow
+		}
+		fmt.Println(result)
+	case "-":
+		result := val1 - val2
+		if (val1 > 0 && val2 < 0 && result < 0) || (val1 < 0 && val2 > 0 && result > 0) {
+			return // Overflow
+		}
+		fmt.Println(result)
+	case "*":
+		result := val1 * val2
+		if val1 != 0 && result/val1 != val2 {
+			return // Overflow
+		}
+		fmt.Println(result)
+	case "/":
+		if val2 == 0 {
+			fmt.Println("No division by 0")
+		} else {
+			fmt.Println(val1 / val2)
+		}
+	case "%":
+		if val2 == 0 {
+			fmt.Println("No modulo by 0")
+		} else {
+			fmt.Println(val1 % val2)
+		}
+	default:
+		return
+	}
 }
