5c7179204d2bedd2f725145637c3ae0b4f1c3e36
bosterma <nalle.osterman@gmail.com>
2024-05-23 08:07:35 +0300

    An enigma

---
+++ enigma.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	// Temporary storage for the values to swap them around
+	tempA := ***a
+	tempB := *b
+	tempC := *******c
+	tempD := ****d
+
+	// Move values around as specified
+	*******c = tempA
+	****d = tempC
+	*b = tempD
+	***a = tempB
+}
