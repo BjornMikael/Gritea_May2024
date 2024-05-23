d7a560409ab0c787c05e629e38ad912bc22dda56
bosterma <nalle.osterman@gmail.com>
2024-05-23 04:30:30 +0300

    collatzcountdown being sent. X

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -2,7 +2,7 @@
 package piscine
 
 // CollatzCountdown returns the number of steps to reach 1 using the Collatz sequence.
-// It returns -1 if then the start is zero or negative.
+// It returns -1 if happdthen the start is zero or negative.
 func CollatzCountdown(start int) int {
 	if start <= 0 {
 		return -1
