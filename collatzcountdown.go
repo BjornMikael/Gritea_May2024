8a94a3914305c2176ef01a80526e5bdaa1c307a4
bosterma <nalle.osterman@gmail.com>
2024-05-23 04:20:11 +0300

    collatzcountdown for hacka thorn  X

---
+++ collatzcountdown.go
@@ -0,0 +1,21 @@
+package piscine
+
+// CollatzCountdown returns the number of steps to reach 1 using the Collatz sequence.
+// It returns -1 if the start is zero or negative.
+func CollatzCountdown(start int) int {
+	if start <= 0 {
+		return -1
+	}
+
+	steps := 0
+	for start != 1 {
+		if start%2 == 0 {
+			start /= 2
+		} else {
+			start = start*3 + 1
+		}
+		steps++
+	}
+
+	return steps
+}
