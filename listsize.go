524abd68eef934d9fb2caec927156fd1aa4d2171
bosterma <nalle.osterman@gmail.com>
2024-05-26 17:55:18 +0300

    List Size; q11t3

--- listsize.go
+++ listsize.go
@@ -1,6 +1,6 @@
 package piscine
 
-// ListSize returns the number of elements in this list
+// ListSize returns the number of elements in the list
 func ListSize(l *List) int {
 	n := l.Head
 	size := 0
