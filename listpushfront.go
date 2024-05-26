401af25be9d9ee93a4b25052538e77fcaff21420
bosterma <nalle.osterman@gmail.com>
2024-05-26 13:59:36 +0300

    List Push Front; q11t2

--- listpushfront.go
+++ listpushfront.go
@@ -1,30 +1,12 @@
 package piscine
 
-// NodeL represents a node in the linked list
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-// List represents the linked list itself
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
-// ListPushFront inserts a new element at the beginning of the list
 func ListPushFront(l *List, data interface{}) {
-	// Create a new node with the given data
-	newNode := &NodeL{
-		Data: data,
-		Next: l.Head,
-	}
-
-	// Update the head of the list to be the new node
-	l.Head = newNode
-
-	// If the list was empty, set the tail to the new node as well
-	if l.Tail == nil {
-		l.Tail = newNode
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	} else {
+		n.Next = l.Head
+		l.Head = n
 	}
 }
